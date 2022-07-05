cfa2f68cd4d6c93064927634f071f477db38f9cb
Valerio Barrila <valerio.barrila@gmail.com>
2022-07-05 21:49:17 +0200

    remove vendor folder

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func getConfig() *viper.Viper {
+	config := viper.New()
+
+	config.SetConfigName("config.yaml")
+	config.AddConfigPath(".")
+	config.SetConfigType("yaml")
+	config.AutomaticEnv()
+	config.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := config.ReadInConfig()
+	if err != nil {
+		log.Error("Error loading config")
+		log.Error(err)
+	}
+
+	return config
+}
+
+func setupRouter(config *viper.Viper) *gin.Engine {
+	gin.SetMode(config.GetString("gin.release_mode"))
+
+	r := gin.Default()
+
+	r.GET("/healthcheck", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	r.POST("/produce", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	return r
+}
 
 func main() {
-	log.Info("heyo i'm an example app that writes to kafka")
+	config := getConfig()
+
+	addr := config.Get("server.address").(string) + ":" + config.Get("server.port").(string)
+	log.Info("Server addr: " + addr)
+
+	r := setupRouter(config)
+	err := r.Run(addr)
+
+	if err != nil {
+		log.Error("Error starting gin server")
+		log.Error(err)
+	}
 }
